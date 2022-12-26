ee6a1cc56bbfd3ce9fe98078232a543afa25dbec
adhishjha <jhaadhish123@gmail.com>
2022-12-26 21:03:03 +0530

    nodejs

---
+++ student.js
@@ -0,0 +1,21 @@
+// console.log("adhish");
+// console.log("i am student");
+
+// const age = 20;
+// const college = "itm";
+
+// module.exports.a = age;
+// module.exports.b = college;
+
+module.exports ={
+    name:"adhish",
+    age:24,
+    city:"gwalior",
+    college:"itm",
+    addition:function(a,b){
+        console.log("its a function",a+b);
+    },
+    substraction:function(a,b){
+        console.log("its a substraction",a-b);
+    }
+}
