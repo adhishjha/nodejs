7f538fe1a101abea5cf1420e7c1f7b8559b515dd
adhishjha <jhaadhish123@gmail.com>
2023-01-02 21:16:11 +0530

    nodejs

--- demo.js
+++ demo.js
@@ -4,4 +4,4 @@ const server = http.createServer(function(req,res){
     res.write('welcome to node js')
     res.write('welcome to gwalior city')
     res.end()
-}).listen(4000,()=>console.log("server is running localhost:4000"))
+}).listen(5500,()=>console.log("server is running localhost:5500"))
